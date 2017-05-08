7f7353d828fd0287820d443fdb2dc191f435ed6e
Dane Kilpatrick <danekilpatrick@Danes-MacBook-Air.local>
2017-05-08 10:36:04 -0500

    Vanilla JS

--- js/app.js
+++ js/app.js
@@ -1,6 +1,35 @@
-// wait for the DOM to finish loading
-$(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
+// Upon loading the game, my functions will be enabled.
+// document.onload = function(){
 
-});
+ // this function will tell me that at the start of the game, player 1('X') is first.
+//   function startGame() {
+//    document.turn = "X";
+//    console.log(document.turn);
+//    setMessage(document.turn + " is first.")
+//   }
+
+// this function will set the message of the class message.
+//  function setMessage(msg) {
+//    var element = document.getElementById("message");
+//    element.innerHTML(msg);
+//   }
+
+// this function will tell me to click another square when trying to choose a square that has already be chosen.
+//  function nextMove(square) {
+//    if(square.innerText === '') {
+//      square.innerText = document.turn;
+//      switchTurn()
+//    } else {
+//      setMessage("Choose another square")
+//    }
+
+// this function will rotate between turns, making it to whenever I choose "X" the next turn will be "O."
+//  function switchTurn() {
+//    if(document.turn === "X") {
+//      document.turn = "O";
+//    } else {
+//      document.turn = "X";
+//    }
+//  }
+//  });
+// }
